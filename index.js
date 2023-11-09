41fcebf6ab659223ca32c10b15f3dfe02fdfe48f
AditiJain1001 <128430112+AditiJain1001@users.noreply.github.com>
2023-11-09 18:05:34 -0500

    Added Index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+// code your own from scratch 
+// make this creative 
+// upload to GitHub side-by-side
+// one path should be rock-paper-sicsors
+// see if you can add UI 
+// https://www.youtube.com/watch?v=ZIw7QGVwaNI
+
+const prompt = require("prompt-sync")();
+
+const answer = prompt("Would you like to play (y/n)? ");
+
+if (answer.toLowerCase() === "y") {
+  const answer2 = prompt(
+    "Would you like to go left or go right (left/right)? "
+  );
+
+  if (answer2 === "left") {
+    console.log("Oops, you go left and fall of a bridge! You lost!");
+  } else {
+    // right
+    console.log("Nice! You go right and approach a bridge...");
+    const answer3 = prompt(
+      "Would you like to cross the bridge, or turn around and find another way (cross/turn)? "
+    );
+
+    if (answer3 === "cross") {
+      console.log("You cross the bridge and reach the end of the game!");
+    } else {
+      console.log(
+        "You turn back and trip on a log and hurt your leg. You lose."
+      );
+    }
+  }
+} else {
+  console.log("That's too bad!");
+}
